16c67a9ac0be991861cc6db92a8c023e2266639b
lunaslash9249@gmail.com <lunaslash9249@gmail.com>
2022-12-14 11:19:17 +0900

    카카오맵 마커추가 완료, 관련 추가내용(정확한 위치, 한글주소 추출 ) 작업중

--- SY_project02/frontend/src/pages/Cart/Cart.jsx
+++ SY_project02/frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,12 @@
 import * as styled_AB from "../../styled/AllBox";
 import Header from "../../components/Header/Header";
-import {Footer} from "../../components/Footer/Footer";
+import { Footer } from "../../components/Footer/Footer";
 import * as styled_F from "../../styled/Favorite";
-import CTitle from "../../image/Cart/CTitle.png"
+import CTitle from "../../image/Cart/CTitle.png";
 import * as styled_C from "../../styled/Cart";
-import {CNoDF} from "./CNoDF";
-import {useState} from "react";
-import {CNoG} from "./CNoG";
+import { CNoDF } from "./CNoDF";
+import { useState } from "react";
+import { CNoG } from "./CNoG";
 
 const Favorite = () => {
   const [state, setState] = useState(1);
@@ -20,122 +20,87 @@ const Favorite = () => {
     2: <CNoG />,
   };
 
-    return (
-        <styled_AB.AllBox>
-            <Header/>
+  return (
+    <styled_AB.AllBox>
+      <Header />
 
-            <styled_F.FBox>
-                {/*My 장바구니 헤더*/}
-                <styled_F.FHeader>
-                    <styled_F.FHTitle>
-                        <styled_F.FHTInner>
-                            <styled_F.FHTInnerH4>
-                                <img src={CTitle} alt={"MyMenu"}/>
-                            </styled_F.FHTInnerH4>
-                        </styled_F.FHTInner>
-                    </styled_F.FHTitle>
-                </styled_F.FHeader>
+      <styled_F.FBox>
+        {/*My 장바구니 헤더*/}
+        <styled_F.FHeader>
+          <styled_F.FHTitle>
+            <styled_F.FHTInner>
+              <styled_F.FHTInnerH4>
+                <img src={CTitle} alt={"MyMenu"} />
+              </styled_F.FHTInnerH4>
+            </styled_F.FHTInner>
+          </styled_F.FHTitle>
+        </styled_F.FHeader>
 
-<<<<<<< HEAD
         {/*My 장바구니 내용*/}
-        <FContentsAllBox>
-          <FCBox>
+        <styled_F.FContentsAllBox>
+          <styled_F.FCBox>
             {/**/}
-            <FCSection1>
-              <FCSectionP>
+            <styled_F.FCSection1>
+              <styled_F.FCSectionP>
                 {/*strong 3개 더 들어가야하는데 샘플이 없어서 못 만드는 중*/}
-              </FCSectionP>
-            </FCSection1>
+              </styled_F.FCSectionP>
+            </styled_F.FCSection1>
             {/*장바구니 내역*/}
-            <FCSection2>
+            <styled_F.FCSection2>
               <div>
-                <CDiv>
-                  <CFoodBox1>
-                    <FCSH5>
+                <styled_C.CDiv>
+                  <styled_C.CFoodBox1>
+                    <styled_F.FCSH5>
                       {state === 1 ? (
-                        <CFoodG href={"#"} onClick={() => onClick(1)}>
+                        <styled_C.CFoodG
+                          href={"#"}
+                          onClick={() => onClick(1)}
+                        >
                           음료/푸드
-                        </CFoodG>
+                        </styled_C.CFoodG>
                       ) : (
-                        <CFood href={"#"} onClick={() => onClick(1)}>
+                        <styled_C.CFood
+                          href={"#"}
+                          onClick={() => onClick(1)}
+                        >
                           음료/푸드
-                        </CFood>
+                        </styled_C.CFood>
                       )}
-                    </FCSH5>
-                  </CFoodBox1>
+                    </styled_F.FCSH5>
+                  </styled_C.CFoodBox1>
 
                   {/*상품 버튼*/}
-                  <CGoodsBox>
-                    <FCSH5>
+                  <styled_C.CGoodsBox>
+                    <styled_F.FCSH5>
                       {state === 2 ? (
-                        <CGoodsG
+                        <styled_C.CGoodsG
                           href={"#"}
                           onClick={() => onClick(2)}
                         >
                           상품
-                        </CGoodsG>
+                        </styled_C.CGoodsG>
                       ) : (
-                        <CGoods href={"#"} onClick={() => onClick(2)}>
+                        <styled_C.CGoods
+                          href={"#"}
+                          onClick={() => onClick(2)}
+                        >
                           상품
-                        </CGoods>
+                        </styled_C.CGoods>
                       )}
-                    </FCSH5>
-                  </CGoodsBox>
-=======
-                {/*My 장바구니 내용*/}
-                <styled_F.FContentsAllBox>
-                    <styled_F.FCBox>
-                        {/**/}
-                        <styled_F.FCSection1>
-                            <styled_F.FCSectionP>
-                                {/*strong 3개 더 들어가야하는데 샘플이 없어서 못 만드는 중*/}
-                            </styled_F.FCSectionP>
-                        </styled_F.FCSection1>
-                        {/*장바구니 내역*/}
-                        <styled_F.FCSection2>
-                            <div>
-                                <styled_C.CDiv>
-                                    <styled_C.CFoodBox1>
-                                        <styled_F.FCSH5>
-                                            {state === 1 ? (
-                                                <styled_C.CFoodG href={"#"} onClick={() => onClick(1)}>
-                                                    음료/푸드
-                                                </styled_C.CFoodG>
-                                            ) : (
-                                                <styled_C.CFood href={"#"} onClick={() => onClick(1)}>
-                                                    음료/푸드
-                                                </styled_C.CFood>
-                                            )}
-                                        </styled_F.FCSH5>
-                                    </styled_C.CFoodBox1>
+                    </styled_F.FCSH5>
+                  </styled_C.CGoodsBox>
 
-                                    {/*상품 버튼*/}
-                                    <styled_C.CGoodsBox>
-                                        <styled_F.FCSH5>
-                                            {state === 2 ? (
-                                                <styled_C.CGoodsG href={"#"} onClick={() => onClick(2)}>
-                                                    상품
-                                                </styled_C.CGoodsG>
-                                            ) : (
-                                                <styled_C.CGoods href={"#"} onClick={() => onClick(2)}>
-                                                    상품
-                                                </styled_C.CGoods>
-                                            )}
-                                        </styled_F.FCSH5>
-                                    </styled_C.CGoodsBox>
->>>>>>> b639be62fec1072b1779ab43e79d271e9e5ef8f6
+                  {obj[state]}
+                </styled_C.CDiv>
+              </div>
+            </styled_F.FCSection2>
+          </styled_F.FCBox>
+        </styled_F.FContentsAllBox>
+      </styled_F.FBox>
 
-                                    {obj[state]}
-                                </styled_C.CDiv>
-                            </div>
-                        </styled_F.FCSection2>
-                    </styled_F.FCBox>
-                </styled_F.FContentsAllBox>
-            </styled_F.FBox>
-
-            <Footer/>
-        </styled_AB.AllBox>
-    );
+      <Footer />
+    </styled_AB.AllBox>
+  );
 };
 
 export default Favorite;
--- SY_project02/frontend/src/pages/Map.jsx
+++ SY_project02/frontend/src/pages/Map.jsx
@@ -1,103 +1,15 @@
-/*global kakao */
 import React from "react";
 import * as styled_AB from "../styled/AllBox";
 import Header from "../components/Header/Header";
 import * as styled_Map from "../styled/Map";
 import { Footer } from "../components/Footer/Footer";
+import { Location } from "./Location";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import MBImg from "../image/MBImg.png";
-import { MapMarker, Map } from "react-kakao-maps-sdk";
 
-const Map1 = () => {
-  const [state, setState] = React.useState({
-    center: {
-      lat: 33.450701,
-      lng: 126.570667,
-    },
-    errMsg: null,
-    isLoading: true,
-  });
-
-  const mapScript = document.createElement("script");
-
-  mapScript.async = true;
-  mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_APP_KEY}&autoload=false`;
-
-  document.head.appendChild(mapScript);
-
-  const onLoadKakaoMap = () => {
-    window.kakao.maps.load(() => {
-      const mapContainer = document.getElementById("map");
-      const mapOption = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3, // 지도의 확대 레벨
-      };
-      new window.kakao.maps.Map(mapContainer, mapOption);
-    });
-  };
-  mapScript.addEventListener("load", onLoadKakaoMap);
-
-  React.useEffect(() => {
-    const onGeoOk = (position) => {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-      const API_KEY = "ffd86e9c843e6dd08b0984e54f694a43";
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-      const weather = document.querySelector(
-        "#weather span:first-child"
-      );
-      console.log(weather);
-      const city = document.querySelector("#weather span:last-child");
-      console.log(city);
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          city.innerText = data.name;
-          const weatherEmoji = document.createElement("p");
-          const weatherImg = () => {
-            if (data.weather[0].id >= 801)
-              return (weatherEmoji.InnerText = "☁️");
-            if (data.weather[0].id === 800)
-              return (weatherEmoji.InnerText = "☀️");
-            if (data.weather[0].id >= 200 && data.weather[0].id < 233)
-              return (weatherEmoji.InnerText = "🌩️");
-            if (data.weather[0].id >= 300 && data.weather[0].id < 322)
-              return (weatherEmoji.InnerText = "🌧️");
-            if (data.weather[0].id >= 500 && data.weather[0].id < 532)
-              return (weatherEmoji.InnerText = "🌧️");
-            if (data.weather[0].id >= 600 && data.weather[0].id < 623)
-              return (weatherEmoji.InnerText = "❄️");
-            if (data.weather[0].id >= 701 && data.weather[0].id < 782)
-              return (weatherEmoji.InnerText = "🌫️");
-            if ((data.weather[0].id = 781))
-              return (weatherEmoji.InnerText = "🌪️");
-            else return "";
-          };
-          setState((prev) => ({
-            ...prev,
-            center: {
-              lat: lat, // 위도
-              lng: lon, // 경도
-            },
-            isLoading: false,
-          }));
-          console.log(data.weather[0].id);
-          weather.innerText = `${
-            data.weather[0].main
-          } ${weatherImg()} / ${data.main.temp}`;
-        });
-    };
-    function onGeoError() {
-      alert("현재 위치를 알 수 없어요!.");
-    }
-
-    // if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
-  }, []);
-
+const Map = () => {
   const settings = {
     dots: true,
     infinite: true,
@@ -111,88 +23,55 @@ const Map1 = () => {
   };
 
   return (
-    <styled_AB.AllBox>
-      <Header />
+    <>
+      <styled_AB.AllBox>
+        <Header />
 
-      {/*지도 전체 박스*/}
-      <styled_Map.MBox>
-        {/*지도*/}
-        <styled_Map.MSection>
-          <Map
-            id="map"
-            center={state.center}
-            style={{
-              // 지도의 크기
-              width: "100%",
-              height: "450px",
-            }}
-            level={3}
-          >
-            {/* <MSection
-            id="map"
-            center={state.center}
-            style={{
-              // 지도의 크기
-              width: "100%",
-              height: "450px",
-            }}
-            level={3}
-          > */}
-            {!state.isLoading && (
-              <MapMarker position={state.center}>
-                <div style={{ padding: "5px", color: "#000" }}>
-                  {state.errMsg ? state.errMsg : "여기에 계신가요?!"}
-                </div>
-              </MapMarker>
-            )}
-            {/* </MSection> */}
-          </Map>
-          <div id="weather">
-            <span></span>
-            <span></span>
-          </div>
-        </styled_Map.MSection>
-
-        {/*매장에서 진행되는 이벤트*/}
-        <styled_Map.MBSection>
-          <styled_Map.MBSP>매장에서 진행되는 이벤트</styled_Map.MBSP>
-          <styled_Map.MBSDiv>
-            <styled_Map.MSDDiv>
-              <Slider {...settings}>
-                <styled_Map.MSDDLi>
-                  <styled_Map.MSDDLImg src={MBImg} alt={"MBImg"} />
-                  <styled_Map.MSDDLP>
-                    준비중
-                    <br />
-                    <styled_Map.MSDDLPSpan>-</styled_Map.MSDDLPSpan>
-                  </styled_Map.MSDDLP>
-                </styled_Map.MSDDLi>
-                <styled_Map.MSDDLi>
-                  <styled_Map.MSDDLImg src={MBImg} alt={"MBImg"} />
-                  <styled_Map.MSDDLP>
-                    준비중
-                    <br />
-                    <styled_Map.MSDDLPSpan>-</styled_Map.MSDDLPSpan>
-                  </styled_Map.MSDDLP>
-                </styled_Map.MSDDLi>
-                <styled_Map.MSDDLi>
-                  <styled_Map.MSDDLImg src={MBImg} alt={"MBImg"} />
-                  <styled_Map.MSDDLP>
-                    준비중
-                    <br />
-                    <styled_Map.MSDDLPSpan>-</styled_Map.MSDDLPSpan>
-                  </styled_Map.MSDDLP>
-                </styled_Map.MSDDLi>
-              </Slider>
-            </styled_Map.MSDDiv>
-            {/*이전, 다음 버튼*/}
-            <styled_Map.MSButtonBox></styled_Map.MSButtonBox>
-          </styled_Map.MBSDiv>
-        </styled_Map.MBSection>
-      </styled_Map.MBox>
-
-      <Footer />
-    </styled_AB.AllBox>
+        {/*지도 전체 박스*/}
+        <styled_Map.MBox>
+          <Location />
+          {/*매장에서 진행되는 이벤트*/}
+          <styled_Map.MBSection>
+            <styled_Map.MBSP>
+              매장에서 진행되는 이벤트
+            </styled_Map.MBSP>
+            <styled_Map.MBSDiv>
+              <styled_Map.MSDDiv>
+                <Slider {...settings}>
+                  <styled_Map.MSDDLi>
+                    <styled_Map.MSDDLImg src={MBImg} alt={"MBImg"} />
+                    <styled_Map.MSDDLP>
+                      준비중
+                      <br />
+                      <styled_Map.MSDDLPSpan>-</styled_Map.MSDDLPSpan>
+                    </styled_Map.MSDDLP>
+                  </styled_Map.MSDDLi>
+                  <styled_Map.MSDDLi>
+                    <styled_Map.MSDDLImg src={MBImg} alt={"MBImg"} />
+                    <styled_Map.MSDDLP>
+                      준비중
+                      <br />
+                      <styled_Map.MSDDLPSpan>-</styled_Map.MSDDLPSpan>
+                    </styled_Map.MSDDLP>
+                  </styled_Map.MSDDLi>
+                  <styled_Map.MSDDLi>
+                    <styled_Map.MSDDLImg src={MBImg} alt={"MBImg"} />
+                    <styled_Map.MSDDLP>
+                      준비중
+                      <br />
+                      <styled_Map.MSDDLPSpan>-</styled_Map.MSDDLPSpan>
+                    </styled_Map.MSDDLP>
+                  </styled_Map.MSDDLi>
+                </Slider>
+              </styled_Map.MSDDiv>
+              {/*이전, 다음 버튼*/}
+              <styled_Map.MSButtonBox></styled_Map.MSButtonBox>
+            </styled_Map.MBSDiv>
+          </styled_Map.MBSection>
+        </styled_Map.MBox>
+        <Footer />
+      </styled_AB.AllBox>
+    </>
   );
 };
-export default Map1;
+export default Map;
