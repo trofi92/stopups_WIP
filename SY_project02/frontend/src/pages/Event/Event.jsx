2c0ce3e25ac214d727f034d7136fc0097321a6b3
hyesu2 <zpswis09@gmail.com>
2022-12-15 11:24:17 +0900

    api 주소 변경, 사이트 전체 width 및 가운데 정렬

--- SY_project02/frontend/src/pages/Event/Event.jsx
+++ SY_project02/frontend/src/pages/Event/Event.jsx
@@ -9,32 +9,31 @@ import {EAll} from "./EAll";
 import {ESCard} from "./ESCard";
 import {ESReward} from "./ESReward";
 import {EOnline} from "./EOnline";
-import {useParams} from "react-router-dom";
 
 axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
 axios.defaults.withCredentials = false;
 
 const Event = () => {
     const [state, setState] = useState(1);
-    const [eventImg, setEventImg] = useState()
+    const [eventImg, setEventImg] = useState();
 
     const onClick = (id) => {
         setState(id);
     };
 
-  const obj = {
-    1: <EAll />,
-    2: <ESCard />,
-    3: <ESReward />,
-    4: <EOnline />,
-  };
+    const obj = {
+        1: <EAll/>,
+        2: <ESCard/>,
+        3: <ESReward/>,
+        4: <EOnline/>,
+    };
 
     const eventApi = async () => {
         // 이미지 조회 시 카테고리 지정 및 검색 값에 따른 DB 조회
         const event = await axios.get(
-            "http://stopupsapi.shop:8080/api/?apikey=TeamYN1670470710431&MImage=ALL&Name=&ImageId="
+            "http://stopupsapi.shop:8080/api/?apikey=TeamYN1670470710431&Event=ALL&Name=&EventId="
         );
-        console.log(event);
+        console.log("event=>", event);
     };
 
     useEffect(() => {
@@ -42,101 +41,101 @@ const Event = () => {
     }, []);
 
 
-  return (
-    <styled_AB.AllBox>
-      <Header />
+    return (
+        <styled_AB.AllBox>
+            <Header/>
 
-      {/*이벤트 타이틀*/}
-      <styled_Notice.NTitleBox>
-        <styled_Notice.NTInner>
-          <styled_Notice.NTIH2>
-            <styled_Notice.NTIImg src={ETitle} alt={"ETitle"} />
-          </styled_Notice.NTIH2>
-        </styled_Notice.NTInner>
-      </styled_Notice.NTitleBox>
-      {/*이벤트 전체 박스*/}
-      <styled_Event.EBox>
-        <styled_Event.EBPosition>
-          <styled_Event.ESection>
-            {/*이벤트 종류 버튼 박스*/}
-            <styled_Event.ESDiv />
-            {/*이벤트 종류 버튼*/}
-            <styled_Event.ESH31>
-              {/*클릭된 이벤트 종류*/}
-              {state === 1 ? (
-                <styled_Event.ESA1G
-                  href={"#"}
-                  onClick={() => onClick(1)}
-                >
-                  전체
-                </styled_Event.ESA1G>
-              ) : (
-                <styled_Event.ESA1
-                  href={"#"}
-                  onClick={() => onClick(1)}
-                >
-                  전체
-                </styled_Event.ESA1>
-              )}
-            </styled_Event.ESH31>
-            <styled_Event.ESH32>
-              {state === 2 ? (
-                <styled_Event.ESA2G
-                  href={"#"}
-                  onClick={() => onClick(2)}
-                >
-                  스탑업스 카드
-                </styled_Event.ESA2G>
-              ) : (
-                <styled_Event.ESA2
-                  href={"#"}
-                  onClick={() => onClick(2)}
-                >
-                  스탑업스 카드
-                </styled_Event.ESA2>
-              )}
-            </styled_Event.ESH32>
-            <styled_Event.ESH33>
-              {state === 3 ? (
-                <styled_Event.ESA3G
-                  href={"#"}
-                  onClick={() => onClick(3)}
-                >
-                  스탑업스 리워드
-                </styled_Event.ESA3G>
-              ) : (
-                <styled_Event.ESA3
-                  href={"#"}
-                  onClick={() => onClick(3)}
-                >
-                  스탑업스 리워드
-                </styled_Event.ESA3>
-              )}
-            </styled_Event.ESH33>
-            <styled_Event.ESH34>
-              {state === 4 ? (
-                <styled_Event.ESA4G
-                  href={"#"}
-                  onClick={() => onClick(4)}
-                >
-                  온라인
-                </styled_Event.ESA4G>
-              ) : (
-                <styled_Event.ESA4
-                  href={"#"}
-                  onClick={() => onClick(4)}
-                >
-                  온라인
-                </styled_Event.ESA4>
-              )}
-            </styled_Event.ESH34>
+            {/*이벤트 타이틀*/}
+            <styled_Notice.NTitleBox>
+                <styled_Notice.NTInner>
+                    <styled_Notice.NTIH2>
+                        <styled_Notice.NTIImg src={ETitle} alt={"ETitle"}/>
+                    </styled_Notice.NTIH2>
+                </styled_Notice.NTInner>
+            </styled_Notice.NTitleBox>
+            {/*이벤트 전체 박스*/}
+            <styled_Event.EBox>
+                <styled_Event.EBPosition>
+                    <styled_Event.ESection>
+                        {/*이벤트 종류 버튼 박스*/}
+                        <styled_Event.ESDiv/>
+                        {/*이벤트 종류 버튼*/}
+                        <styled_Event.ESH31>
+                            {/*클릭된 이벤트 종류*/}
+                            {state === 1 ? (
+                                <styled_Event.ESA1G
+                                    href={"#"}
+                                    onClick={() => onClick(1)}
+                                >
+                                    전체
+                                </styled_Event.ESA1G>
+                            ) : (
+                                <styled_Event.ESA1
+                                    href={"#"}
+                                    onClick={() => onClick(1)}
+                                >
+                                    전체
+                                </styled_Event.ESA1>
+                            )}
+                        </styled_Event.ESH31>
+                        <styled_Event.ESH32>
+                            {state === 2 ? (
+                                <styled_Event.ESA2G
+                                    href={"#"}
+                                    onClick={() => onClick(2)}
+                                >
+                                    스탑업스 카드
+                                </styled_Event.ESA2G>
+                            ) : (
+                                <styled_Event.ESA2
+                                    href={"#"}
+                                    onClick={() => onClick(2)}
+                                >
+                                    스탑업스 카드
+                                </styled_Event.ESA2>
+                            )}
+                        </styled_Event.ESH32>
+                        <styled_Event.ESH33>
+                            {state === 3 ? (
+                                <styled_Event.ESA3G
+                                    href={"#"}
+                                    onClick={() => onClick(3)}
+                                >
+                                    스탑업스 리워드
+                                </styled_Event.ESA3G>
+                            ) : (
+                                <styled_Event.ESA3
+                                    href={"#"}
+                                    onClick={() => onClick(3)}
+                                >
+                                    스탑업스 리워드
+                                </styled_Event.ESA3>
+                            )}
+                        </styled_Event.ESH33>
+                        <styled_Event.ESH34>
+                            {state === 4 ? (
+                                <styled_Event.ESA4G
+                                    href={"#"}
+                                    onClick={() => onClick(4)}
+                                >
+                                    온라인
+                                </styled_Event.ESA4G>
+                            ) : (
+                                <styled_Event.ESA4
+                                    href={"#"}
+                                    onClick={() => onClick(4)}
+                                >
+                                    온라인
+                                </styled_Event.ESA4>
+                            )}
+                        </styled_Event.ESH34>
 
-            {obj[state]}
-          </styled_Event.ESection>
-        </styled_Event.EBPosition>
-      </styled_Event.EBox>
-    </styled_AB.AllBox>
-  );
+                        {obj[state]}
+                    </styled_Event.ESection>
+                </styled_Event.EBPosition>
+            </styled_Event.EBox>
+        </styled_AB.AllBox>
+    );
 };
 
 export default Event;
