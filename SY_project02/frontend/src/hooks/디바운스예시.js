ae147945f85e829e4e33e247e58961af06d88750
Bum <Bum9@github.com>
2022-12-23 17:38:57 +0900

    병합(bum)

--- SY_project02/frontend/src/hooks/디바운스예시.js
+++ SY_project02/frontend/src/hooks/디바운스예시.js
@@ -1,43 +1,43 @@
-import React from "react";
-import { useDebounce } from "./use-debounce";
+// import React from "react";
+// import { useDebounce } from "./use-debounce";
 
-const DATA = [
-  "세상",
-  "마상",
-  "선생님",
-  "프로젝트",
-  "버스",
-  "피곤",
-  "cu편의점",
-  "custom",
-];
+// const DATA = [
+//   "세상",
+//   "마상",
+//   "선생님",
+//   "프로젝트",
+//   "버스",
+//   "피곤",
+//   "cu편의점",
+//   "custom",
+// ];
 
-export default function 디바운스예시() {
-  const [results, setResults] = React.useState([]);
-  const [text, setText] = React.useState("");
-  // text = val
-  // 500 = delay
-  const deb = useDebounce(text, 500);
+// export default function 디바운스예시() {
+//   const [results, setResults] = React.useState([]);
+//   const [text, setText] = React.useState("");
+//   // text = val
+//   // 500 = delay
+//   const deb = useDebounce(text, 500);
 
-  React.useEffect(() => {
-    const searchData = DATA.filter((el) =>
-      el.toLowerCase().includes(deb)
-    );
-    setResults(searchData);
-  }, [deb]);
+//   React.useEffect(() => {
+//     const searchData = DATA.filter((el) =>
+//       el.toLowerCase().includes(deb)
+//     );
+//     setResults(searchData);
+//   }, [deb]);
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      {results.length > 0 ? (
-        results.map((el, idx) => <div key={idx}>{el}</div>)
-      ) : (
-        <div>no results</div>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         value={text}
+//         onChange={(e) => setText(e.target.value)}
+//       />
+//       {results.length > 0 ? (
+//         results.map((el, idx) => <div key={idx}>{el}</div>)
+//       ) : (
+//         <div>no results</div>
+//       )}
+//     </div>
+//   );
+// }
