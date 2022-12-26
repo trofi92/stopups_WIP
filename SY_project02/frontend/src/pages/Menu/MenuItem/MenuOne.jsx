6705574547e2b3fbde8c11296ff592fc07fb9656
hyesu2 <zpswis09@gmail.com>
2022-12-26 09:14:51 +0900

    menu redux 수정, cart redux 수정 예정

--- SY_project02/frontend/src/pages/Menu/MenuItem/MenuOne.jsx
+++ SY_project02/frontend/src/pages/Menu/MenuItem/MenuOne.jsx
@@ -6,14 +6,25 @@ import * as styled_Menu from "../../../styled/Menu/Menu";
 import * as styled_MenuItem from "../../../styled/Menu/MenuItem";
 import {ButtonSmallBox} from "../../../styled/Button";
 import {useDispatch, useSelector} from "react-redux";
-import {createSlice} from "@reduxjs/toolkit";
-import {addToItem} from "../../../features/cart/cartSlice";
+import {addToCart} from "../../../features/cart/cartSlice";
+import {addToFavorites} from "../../../features/favorite/favoriteSlice";
 
 const DetailOne = (props) => {
-    const params = useParams();
     const [sizeData, setSizeData] = useState("");
-    const cart = useSelector((state) => state.cart.cartItems);
     const total = useSelector((state) => state.cart.total);
+    const replaceNumber = (value) => value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    let now = new Date();
+    let year = now.getFullYear();
+    let todayMont = now.getMonth() + 1;
+    let todayDate = now.getDate();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    const whatDateTime = `${year}-${todayMont}-${todayDate} ${hour}:${minutes}:${seconds}`;
+
+    const dispatch = useDispatch();
+    const params = useParams();
 
     const InValid =
         props.price.Desert !== "0" && props.price.hasOwnProperty("Desert");
@@ -28,33 +39,66 @@ const DetailOne = (props) => {
         props.category === "샌드위치" ||
         props.category === "따뜻한 푸드" ||
         props.category === "샐러드";
-    const onSubmitHandler = (e) => {
+
+    // const onSubmitHandler = (e) => {
+    //     if (sizeData === "") {
+    //         alert("사이즈를 선택해주세요!");
+    //         e.preventDefault();
+    //     } else {
+    //         e.preventDefault();
+    //
+    //         dispatch(
+    //             addToItem({
+    //                 id: props.productId,
+    //                 name: props.name,
+    //                 price: props.price[sizeData],
+    //                 size: sizeData,
+    //             })
+    //         );
+    //     }
+    //
+    //     console.log(props.productId, props.price, props);
+    // };
+
+    const onClickFavorite = (e) => {
         if (sizeData === "") {
-            alert("사이즈를 선택해주세요!");
             e.preventDefault();
+            alert("사이즈를 선택해주세요.");
         } else {
             e.preventDefault();
-
-            dispatch(
-                addToItem({
-                    id: props.productId,
-                    name: props.name,
-                    price: props.price[sizeData],
-                    size: sizeData,
-                })
-            );
+            dispatch(addToFavorites({
+                id: props.productId,
+                name: props.name,
+                size: sizeData,
+                price: props.price[sizeData],
+                whatDateTime: whatDateTime,
+                category: props.category,
+                amount: 1,
+            }))
+            alert("나만의 음료에 등록했습니다.")
+            // 새로고침 되면서 favorite에 들어간 내용들이 전부 초기화됨....
+            // if(window.confirm("나만의 음료에 등록했습니다. My 메뉴로 이동하시겠습니까?")) {
+            //     window.location.href = "/favorite"
+            // }
         }
+    }
 
-        console.log(props.productId, props.price, props);
-    };
-
-    const testHandler = () => {
-        console.log(cart);
-    };
-
-    const dispatch = useDispatch();
-
-    const replaceNumber = (value) => value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    const onClickCart = (e) => {
+        if (sizeData === "") {
+            e.preventDefault();
+            alert("사이즈를 선택해주세요.");
+        } else {
+            e.preventDefault();
+            dispatch(addToCart({
+                id: props.productId,
+                name: props.name,
+                size: sizeData,
+                price: props.price[sizeData],
+                category: props.category
+            }))
+            alert("장바구니에 등록했습니다.")
+        }
+    }
 
     return (
         <AllBox>
@@ -158,11 +202,11 @@ const DetailOne = (props) => {
                                     <p>나만의 푸드로 등록</p>
                                 </ButtonSmallBox>
                             ) : (
-                                <ButtonSmallBox onClick={onSubmitHandler}>
+                                <ButtonSmallBox onClick={onClickFavorite}>
                                     <p>나만의 음료로 등록</p>
                                 </ButtonSmallBox>
                             )}
-                            <ButtonSmallBox onClick={testHandler}>
+                            <ButtonSmallBox onClick={onClickCart}>
                                 <p>장바구니 등록</p>
                             </ButtonSmallBox>
                         </styled_Menu.ButtonBoxCotainer>
