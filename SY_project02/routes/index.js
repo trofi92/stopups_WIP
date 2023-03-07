dee52fb9b20da86edd2f8442efade72bf8797dbb
trofi92 <lunaslash9249@gmail.com>
2023-03-07 23:56:57 +0900

    불필요한 코드 삭제

--- SY_project02/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res, next) => {
-  res.send({ message: "hello root" });
-  next();
-});
-
-module.exports = router;
